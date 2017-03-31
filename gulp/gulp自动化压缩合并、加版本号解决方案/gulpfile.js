e8acc821731e0da2277bf4acf64a2c686fd31f85
illuSioN_uestc <413972057@qq.com>
2017-03-31 10:34:26 +0800

    gulp 自动化压缩合并+版本号

---
+++ gulp/gulp自动化压缩合并、加版本号解决方案/gulpfile.js
@@ -0,0 +1,47 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: illuSioN4ng
+ * Date: 2017/3/27
+ * Description:
+ */
+// gulpfile.js
+var path = require('path'),
+    gulp = require('gulp'),
+    htmlmini = require('gulp-html-minify'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    csso = require('gulp-csso'),
+    filter = require('gulp-filter'),
+    RevAll = require('gulp-rev-all'),
+    del = require('del');
+
+gulp.task('default',['del'], function () {
+    var jsFilter = filter('dev/**/*.js',{restore:true}),
+        cssFilter = filter('dev/**/*.css',{restore:true}),
+        htmlFilter = filter(['dev/**/*.html'],{restore:true});
+    gulp.src('dev/index.html')
+        .pipe(useref())                         // 解析html中的构建块
+        .pipe(jsFilter)                         // 过滤所有js
+        .pipe(uglify())                         // 压缩js
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)                        // 过滤所有css
+        .pipe(csso())                           // 压缩优化css
+        .pipe(cssFilter.restore)
+        .pipe(RevAll.revision({                 // 生成版本号
+            dontRenameFile: ['.html'],          // 不给 html 文件添加版本号
+            dontUpdateReference: ['.html'],      // 不给文件里链接的html加版本号
+            transformFilename: function (file, hash) {
+                // var ext = path.extname(file.path);
+                // return hash.substr(0, 5) + '.'  + path.basename(file.path, ext) + ext; // 3410c.filename.ext
+                return hash.substr(0, 16) + path.extname(file.path);
+            }
+        }))
+        .pipe(htmlFilter)                       // 过滤所有html
+        .pipe(htmlmini())                       // 压缩html
+        .pipe(htmlFilter.restore)
+        .pipe(gulp.dest('./test'));
+});
+
+gulp.task('del',function () {
+    del('./test/*');                               // 构建前先删除dist文件里的旧版本
+});
---
+++ gulp/gulp自动化压缩合并、加版本号解决方案/readme.md
@@ -0,0 +1,9 @@
+## gulp 自动化压缩合并加版本号
+&emsp;&emsp;首先需要根据引入的npm包依次`npm install (gulp-filter gulp-useref ...)`
+### [gulp-filter](https://www.npmjs.com/package/gulp-filter)
+### [gulp-useref](https://www.npmjs.com/package/gulp-useref)
+### [gulp-uglify](https://www.npmjs.com/package/gulp-uglify)
+### [gulp-csso](https://www.npmjs.com/package/gulp-csso)
+### [gulp-html-minify](https://www.npmjs.com/package/gulp-html-minify)
+### [gulp-rev-all](https://www.npmjs.com/package/gulp-rev-all)
+### [del](https://www.npmjs.com/package/del)
